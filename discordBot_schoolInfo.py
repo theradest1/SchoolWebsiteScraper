52ee70b3218991ed8ab00907827fbcf69902b623
TheRad1 <18326@verona.k12.wi.us>
2023-01-19 16:28:21 -0600

    a lot
    added hidden info, cleaned up code, and started on discord bot part

--- discordBot_schoolInfo.py
+++ discordBot_schoolInfo.py
@@ -1,31 +1,50 @@
+##!/usr/bin/python3
+
+import discord
+from discord.ext import commands
 from selenium import webdriver
 from datetime import datetime
 from selenium.webdriver.common.by import By
 import time
 
-today = datetime.today().strftime('%Y-%m-%d').replace("-", "")
-print("Date (URL format): " + today)
-vahsURL = "https://www.verona.k12.wi.us/Page/28#calendar16/" + today + "/day"
-print("URL used: " + vahsURL)
-classToFind = "wcm-calendar-event-title"
+#accessing hidden info that I don't want online
+hiddenInfoFile = open("hiddenInfo.txt")
+hiddenInfo = content = hiddenInfoFile.readlines()
 
-browser = webdriver.Firefox()
-browser.get(vahsURL)
-print("waiting for page to load...")
-time.sleep(5)
-print("scrapping page...")
-elements = browser.find_elements(By.CLASS_NAME, classToFind)
-print("parsing info...")
+#discord bot init
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="@", intents = intents)
 
-evenDays = False
+#settings
+timeToLoad = 120 #in seconds (higher is better but takes more time for testing)
+
+schoolWebsite = hiddenInfo[3] + getCurrentDate() + "/day"
+
+#print("Date: " + getCurrentDate())
+#print("URL used: " + schoolWebsite)
+
+elements = scrape(schoolWebsite, "wcm-calendar-event-title")
+
+print("Today's info: ")
 for element in elements:
-    text = element.text
-    if(text == "Periods 2, 4, 6, 7"):
-        evenDays = True
-browser.close()
+    print(element.text)
 
-if(evenDays):
-    print("Today is an even day")
-else:
-    print("Today is an odd day")
-exit()
+@bot.command()
+async def die(ctx):
+	await ctx.send("Bye (:")
+	exit()
+
+def getCurrentDate():
+    return datetime.today().strftime('%Y-%m-%d').replace("-", "")
+
+def scrape(websiteURL, className):
+    browser = webdriver.Firefox()
+    browser.get(vahsURL)
+    time.sleep(timeToLoad)
+    try:
+        return browser.find_elements(By.CLASS_NAME, classToFind)
+    except:
+        return []
+
+bot.run(hiddenInfo[1])
