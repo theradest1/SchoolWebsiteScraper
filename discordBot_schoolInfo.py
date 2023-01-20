f81899586d2b03019a591578f73ba4fb2563fa35
TheRad1 <18326@verona.k12.wi.us>
2023-01-20 15:42:29 -0600

    python threading is dogwater

--- discordBot_schoolInfo.py
+++ discordBot_schoolInfo.py
@@ -4,16 +4,19 @@ from selenium import webdriver
 from datetime import datetime
 from selenium.webdriver.common.by import By
 import time
+import threading
 import asyncio
 
 #discord bot init
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix="@", intents = intents)
-        
-async def nextDay():
+
+async def dayChecker():
+    
     print("hola")
     elements, browser = scrape(schoolWebsite, "wcm-calendar-event-title")
+    print(schoolWebsite)
     await bot.get_channel(1065857282040152114).send("Events for today:")
     for element in elements:
         await bot.get_channel(1065857282040152114).send(element.text)
@@ -33,6 +36,24 @@ def scrape(websiteURL, className):
         elements = []
     return elements, browser
 
+#@bot.event
+#async def on_message(message):
+#    global schoolWebsite
+#    if message.content == "events":
+#        elements, browser = await scrape(schoolWebsite, "wcm-calendar-event-title")
+#        #await message.channel.send("Events for today:")
+#        print("send: Events for today:")
+#        for element in elements:
+#            #await message.channel.send(element.text)
+#            print("send: " + element.text)
+#        browser.close()
+        
+@bot.command()
+async def die(ctx):
+    await ctx.send("Bye (:")
+    botThread._stop()
+    exit()
+
 #accessing hidden info that I don't want online
 hiddenInfoFile = open("hiddenInfo.txt")
 hiddenInfo = content = hiddenInfoFile.readlines()
@@ -41,31 +62,18 @@ hiddenInfo = content = hiddenInfoFile.readlines()
 schoolWebsite = hiddenInfo[3] + getCurrentDate() + "/day"
 print(schoolWebsite)
 timeBetweenChecks = 1000
-timeToLoad = 10 #in seconds (higher is better but takes more time for testing)
+timeToLoad = 5 #in seconds (higher is better but takes more time for testing)
 
-@bot.event
-async def on_message(message):
-    global schoolWebsite
-    if message.content == "events":
-        elements, browser = scrape(schoolWebsite, "wcm-calendar-event-title")
-        await message.channel.send("Events for today:")
-        for element in elements:
-            await message.channel.send(element.text)
-        browser.close()
-    if message.content == "startEvents":
-        await message.channel.send("aight")
-        date = ""   
-        while True:
-            print("testing day")
-            if date != getCurrentDate():
-                await nextDay()
-            else:
-                date = getCurrentDate()
-            time.sleep(timeBetweenChecks)
-        
-@bot.command()
-async def die(ctx):
-	await ctx.send("Bye (:")
-	exit()
+print("starting discord bot")
+botThread = threading.Thread(target=bot.run, args=(hiddenInfo[1],))
+botThread.start()
 
-bot.run(hiddenInfo[1])
+print("starting main loop")
+date = ""   
+while True:
+    print("testing day")
+    if date != getCurrentDate():
+        asyncio.run(nextDay())
+        date = getCurrentDate()
+    else:
+        date = getCurrentDate()
