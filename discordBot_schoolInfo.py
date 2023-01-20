2e654fba9298f32ee7c734b2c4b5faf69b3f0f6e
TheRad1 <18326@verona.k12.wi.us>
2023-01-20 09:53:30 -0600

    pre-trying js to scrape because python async is dog

--- discordBot_schoolInfo.py
+++ discordBot_schoolInfo.py
@@ -1,50 +1,71 @@
-##!/usr/bin/python3
-
 import discord
 from discord.ext import commands
 from selenium import webdriver
 from datetime import datetime
 from selenium.webdriver.common.by import By
 import time
-
-#accessing hidden info that I don't want online
-hiddenInfoFile = open("hiddenInfo.txt")
-hiddenInfo = content = hiddenInfoFile.readlines()
+import asyncio
 
 #discord bot init
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix="@", intents = intents)
-
-#settings
-timeToLoad = 120 #in seconds (higher is better but takes more time for testing)
-
-schoolWebsite = hiddenInfo[3] + getCurrentDate() + "/day"
-
-#print("Date: " + getCurrentDate())
-#print("URL used: " + schoolWebsite)
-
-elements = scrape(schoolWebsite, "wcm-calendar-event-title")
-
-print("Today's info: ")
-for element in elements:
-    print(element.text)
-
-@bot.command()
-async def die(ctx):
-	await ctx.send("Bye (:")
-	exit()
+        
+async def nextDay():
+    print("hola")
+    elements, browser = scrape(schoolWebsite, "wcm-calendar-event-title")
+    await bot.get_channel(1065857282040152114).send("Events for today:")
+    for element in elements:
+        await bot.get_channel(1065857282040152114).send(element.text)
+    browser.close()
 
 def getCurrentDate():
     return datetime.today().strftime('%Y-%m-%d').replace("-", "")
 
 def scrape(websiteURL, className):
     browser = webdriver.Firefox()
-    browser.get(vahsURL)
+    browser.get(websiteURL)
     time.sleep(timeToLoad)
     try:
-        return browser.find_elements(By.CLASS_NAME, classToFind)
+        elements = browser.find_elements(By.CLASS_NAME, className)
     except:
-        return []
+        print("browser failed to get any events from webpage")
+        elements = []
+    return elements, browser
+
+#accessing hidden info that I don't want online
+hiddenInfoFile = open("hiddenInfo.txt")
+hiddenInfo = content = hiddenInfoFile.readlines()
+
+#settings
+schoolWebsite = hiddenInfo[3] + getCurrentDate() + "/day"
+print(schoolWebsite)
+timeBetweenChecks = 1000
+timeToLoad = 10 #in seconds (higher is better but takes more time for testing)
+
+@bot.event
+async def on_message(message):
+    global schoolWebsite
+    if message.content == "events":
+        elements, browser = scrape(schoolWebsite, "wcm-calendar-event-title")
+        await message.channel.send("Events for today:")
+        for element in elements:
+            await message.channel.send(element.text)
+        browser.close()
+    if message.content == "startEvents":
+        await message.channel.send("aight")
+        date = ""   
+        while True:
+            print("testing day")
+            if date != getCurrentDate():
+                await nextDay()
+            else:
+                date = getCurrentDate()
+            time.sleep(timeBetweenChecks)
+        
+@bot.command()
+async def die(ctx):
+	await ctx.send("Bye (:")
+	exit()
 
 bot.run(hiddenInfo[1])
