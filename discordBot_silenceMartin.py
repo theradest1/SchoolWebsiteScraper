b4bc36c2b8ea95c64d2a84f0a7d0b46846998943
TheRad1 <18326@verona.k12.wi.us>
2023-01-25 15:37:23 -0600

    changed gitignore and testing things

--- discordBot_silenceMartin.py
+++ discordBot_silenceMartin.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+import random
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -19,13 +20,14 @@ async def on_ready():
 async def on_message(message):
     global silencingMartin
     if bot.user.id != message.author.id:
-        if message.author.id == 594245967738437639 and silencingMartin == True:
-            await message.delete()
-            return
-        if message.content == "brug":
-            await message.channel.send("in a rug")
-            
-
+        if message.author.id == 594245967738437639:
+            if silencingMartin == True:
+                await message.delete()
+                return
+            elif random.randint(0, 70) == 69:
+                await message.delete()
+                await message.channel.send("Martin said something but I deleted it lol")
+                await message.channel.send("Skill issue :shrug:")
     await bot.process_commands(message)
 
 @bot.command()
@@ -36,5 +38,7 @@ async def silenceMartin(ctx):
     else:
         await ctx.channel.send("The silence has been toggled on... thank god")
     silencingMartin = not silencingMartin
+async def test(ctx):
+    await ctx.channel.send("Skill issue :shrug:")
     
 bot.run(hiddenInfo[1])
