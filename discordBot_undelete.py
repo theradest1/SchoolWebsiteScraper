ea07e3f25c74b3d6f62bb06e0c604c094fed7a11
TheRad1 <18326@verona.k12.wi.us>
2023-04-17 08:29:07 -0500

    added a few things

--- discordBot_undelete.py
+++ discordBot_undelete.py
@@ -8,15 +8,31 @@ bot = commands.Bot(command_prefix=">", intents = intents)
 hiddenInfoFile = open("hiddenInfo.txt")
 hiddenInfo = content = hiddenInfoFile.readlines()
 
+rileyID = 718795668801585203
+channelID = 1097510587208048793
+person = "Riley"
+
+#rileyID = 571446451771801600
+#channelID = 970098568197201932
+#person = "Landon"
+
 @bot.event
 async def on_ready():
     print('client ready')
 
+
 @bot.event
 async def on_message_delete(message):
     if bot.user.id != message.author.id:
-        if message.author.id == 718795668801585203:
-            await message.channel.send("Riley deleted this message: \"" + message.content + "\"")
+        if message.author.id == rileyID:
+            await bot.get_channel(channelID).send(person + " deleted the message: \"" + message.content + "\"")
 
-print(hiddenInfo[1])
+
+@bot.event
+async def on_message_edit(before, after):
+    if bot.user.id != before.author.id:
+        if before.author.id == rileyID:
+            await bot.get_channel(channelID).send(person + " edited the message: \"" + before.content + "\"")
+
+
 bot.run(hiddenInfo[1])
